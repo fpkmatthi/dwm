05c9c5ffd6e10782addc2f21e33e615faae1fda3
Matthias Van De Velde <skymatth@gmail.com>
2021-09-21 00:07:42 +0200

    Add powermenu shortcut

--- config.def.h
+++ config.def.h
@@ -80,11 +80,13 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", norm_bg, "-nf", norm_fg, "-sb", sel_bg, "-sf", sel_fg, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *rofilauncher[]  = { "launcher_misc", NULL };
+static const char *powermenu[]  = { "powermenu", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	/* { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } }, */
 	{ MODKEY,                       XK_p,      spawn,          {.v = rofilauncher } },
+	{ MODKEY,                       XK_x,      spawn,          {.v = powermenu } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
